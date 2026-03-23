f02edc4c006c1962490b65676cf476be5f0c5c91
Viktor <pastivv@gmail.com>
2026-03-23 23:05:40 +0100

    raadzen

--- TikTokTracker.Web/Program.cs
+++ TikTokTracker.Web/Program.cs
@@ -3,12 +3,15 @@ using Microsoft.AspNetCore.DataProtection;
 using TikTokTracker.Web.Components;
 using TikTokTracker.Web.Data;
 using TikTokTracker.Web.Services;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddRadzenComponents();
 
 builder.Services.AddHttpClient("TikTok", client =>
 {
